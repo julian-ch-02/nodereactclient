a030e1bb34c53324059aa81ff20ce7e611ac0d20
julian-ch-02 <julianch36@gmail.com>
2021-11-20 15:56:03 +0700

    delete creds

--- src/App.js
+++ src/App.js
@@ -9,26 +9,8 @@ import PublicRoute from "./route/PublicRoute";
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import { storeToken } from "./authToken";
-import Loading from "./component/Loading";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    axios
-      .post(process.env.REACT_APP_REFRESH_URI, {})
-      .then(async (data) => {
-        const token = await data.data;
-        storeToken(token);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  if (loading) {
-    return <Loading />;
-  }
   return (
     <AuthProvider>
       <BrowserRouter>
